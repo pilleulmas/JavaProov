755a8a36336ac286f8c4b394fd3ac4b5c00d247c
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-29 13:50:57 +0200

    comment

--- src/JavaFX/Joon.java
+++ src/JavaFX/Joon.java
@@ -3,7 +3,7 @@ package JavaFX;
 /**
  * Küsi kasutajalt alguse ja lõpu x-y kordinaadid ning joonista talle joon. Kõik
  * tegevus peab toimuma kasutajaliideses. Akna võime limiteerida 500x500 piksli peale.
- * Kasutajalt võid küsida sisestust nii tekstivälja kui ka hiire klikist.
+ * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
  * Võid ka soovi korral Buttonit või muid abivahendeid kasutada.
  */
 public class Joon {
--- src/JavaFX/Ristkulik.java
+++ src/JavaFX/Ristkulik.java
@@ -3,7 +3,7 @@ package JavaFX;
 /**
  * Küsi kasutajalt alguskordinaadid ja laius-kõrgus ning joonista talle ristkülik. Kõik
  * tegevus peab toimuma kasutajaliideses. Akna võime limiteerida 500x500 piksli peale.
- * Kasutajalt võid küsida sisestust nii tekstivälja kui ka hiire klikist.
+ * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
  * Võid ka soovi korral Buttonit või muid abivahendeid kasutada.
  */
 public class Ristkulik {
