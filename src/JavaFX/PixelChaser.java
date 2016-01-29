aaf7478acc4819af0ae94b2359e9bccedf2aaa84
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-29 18:29:40 +0200

    remove stuff for first day

--- src/JavaFX/PixelChaser.java
+++
@@ -1,9 +0,0 @@
-package JavaFX;
-
-/**
- * Joonista ekraanile hunnik ristkülikuid, erinevatel positsioonidel. Ühe korra kastile klikates
- * muutub ta 2x väiksemaks. Teist korda klikates kaob ta sootuks.
- */
-public class PixelChaser {
-
-}
