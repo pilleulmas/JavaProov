29c16817516e514b0edb6c22d0940e43267edc48
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-31 16:13:15 +0200

    better wording

--- src/JavaFX/PixelChaser.java
+++ src/JavaFX/PixelChaser.java
@@ -1,7 +1,7 @@
 package JavaFX;
 
 /**
- * Joonista ekraanile hunnik ristkülikuid, erinevatel positsioonidel. Ühe korra kastile klikates
+ * Joonista ekraanile hunnik ristkülikuid, suvalistel positsioonidel. Ühe korra kastile klikates
  * muutub ta 2x väiksemaks. Teist korda klikates kaob ta sootuks.
  */
 public class PixelChaser {
