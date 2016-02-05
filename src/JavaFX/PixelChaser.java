252bd48fe25528dee476422a09916d8b96f9d2c5
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-05 14:54:30 +0200

    korduseksamiks valmis

--- src/JavaFX/PixelChaser.java
+++
@@ -1,9 +0,0 @@
-package JavaFX;
-
-/**
- * Joonista ekraanile hunnik ristkülikuid, suvalistel positsioonidel. Ühe korra kastile klikates
- * muutub ta 2x väiksemaks. Teist korda klikates kaob ta sootuks.
- */
-public class PixelChaser {
-
-}
--- src/JavaFX/Ring.java
+++
@@ -1,8 +0,0 @@
-package JavaFX;
-
-/**
- * Küsi kasutajalt x, y kordinaadid ja raadius ning joonista talle ring.
- * Küsi kasutajalt sisendit kuidas soovid, pealselt, et teed seda JavaFXis.
- */
-public class Ring {
-}
